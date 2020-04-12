dd116aecc95f53b55554fb5b07d19c7ea6d32da1
crypcore <50465424+crypcore@users.noreply.github.com>
2020-04-12 22:24:54 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -5,3 +5,5 @@ extern const char* const MONERO_VERSION;
 extern const char* const MONERO_RELEASE_NAME;
 extern const char* const MONERO_VERSION_FULL;
 extern const bool MONERO_VERSION_IS_RELEASE;
+
+
